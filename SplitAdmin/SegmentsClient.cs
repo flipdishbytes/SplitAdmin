18a144a2e3fe9af475ac688a4019e5cff04dde38
Dale Myers <dale.myers@flipdish.com>
2022-10-31 10:45:39 +0000

    Fix segments client

--- SplitAdmin/SegmentsClient.cs
+++ SplitAdmin/SegmentsClient.cs
@@ -10,7 +10,7 @@ namespace SplitAdmin
 {
     public class SegmentsClient : EndpointClient
     {
-        public SegmentsClient(HttpClient client, bool useCache) : base(client, useCache) { }
+        public SegmentsClient(HttpClient client) : base(client) { }
 
         public async Task<IList<Segment>> GetAll(Workspace workspace, string? environmentName = null)
         {
